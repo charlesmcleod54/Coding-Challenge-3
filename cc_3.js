035164e807a5ff7694a633bf68442706342dad88
Charles McLeod <charlesmcleod@usf.edu>
2025-02-02 15:00:31 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -10,3 +10,16 @@ prices.shift();
 
 // Log the updated price list to the console
 console.log(prices);
+// Task 2
+// Declare and initialize the array with at least five numerical values representing quantities
+let orders = [10, 15, 20, 25, 35];
+
+// Increase the third order qunatity by 5
+orders[2] += 5;
+
+// Calculate the total number of all orders
+let totalOrders = orders.reduce((acc, quantity) => acc + quantity, 0);
+
+// Log the updated array and total order count to the console
+console.log("Updated Orders:", orders);
+console.log("Total Order Count:", totalOrders);
